4d6dd5acf78328c726172157c4bc3bf529c92708
jacobAGTyler <jacobagtyler@users.noreply.github.com>
2017-02-21 23:21:25 +0000

    Fixed API namespace issue

--- src/Controller/Api/EmailSendsController.php
+++ src/Controller/Api/EmailSendsController.php
@@ -1,7 +1,7 @@
 <?php
-namespace App\Controller\SuperUser;
+namespace App\Controller\Api;
 
-use App\Controller\SuperUser\AppController;
+use App\Controller\Api\AppController;
 
 /**
  * EmailSends Controller
