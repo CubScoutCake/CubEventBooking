885fe665c29bda482b8667bd9b4d73d551825fa4
jacobAGTyler <jacobagtyler@users.noreply.github.com>
2019-05-06 13:01:20 +0100

    View Changes

--- src/View/AppView.php
+++ src/View/AppView.php
@@ -1,32 +1,35 @@
 <?php
 /**
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link      http://cakephp.org CakePHP(tm) Project
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link      https://cakephp.org CakePHP(tm) Project
  * @since     3.0.0
- * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
  */
 namespace App\View;
 
 use Cake\View\View;
 
 /**
- * App View class
+ * Application View
+ *
+ * Your application’s default view class
+ *
+ * @link https://book.cakephp.org/3.0/en/views.html#the-app-view
  */
 class AppView extends View
 {
-    //public $layout = 'BootstrapUI.default';
-
     /**
      * Initialization hook method.
      *
-     * For e.g. use this method to load a helper for all views:
-     * `$this->loadHelper('Html');`
+     * Use this method to add common initialization code like loading helpers.
+     *
+     * e.g. `$this->loadHelper('Html');`
      *
      * @return void
      */
